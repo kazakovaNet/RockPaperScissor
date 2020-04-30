eb350683dc99ac8d9845d30541eeddbb72887ad0
Казакова Наталья Владимировна <NKazakova@ibs.root.ibscorp.ru>
2020-04-30 09:21:20 +0300

    Initial commit

---
+++ src/Game.kt
@@ -0,0 +1,43 @@
+fun main(args: Array<String>) {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
+}
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+    // Выполнять цикл пока пользователь не введет допустимый вариант
+    while (!isValidChoice) {
+        // Запросить у пользователя его выбор
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+        // Прочитать пользовательский ввод
+        val userInput = readLine()?.capitalize()
+        // Проверить пользовательский ввод
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+        // Если выбран недопустимый вариант, сообщить пользователю
+        if (!isValidChoice) println("You must enter a valid choice")
+    }
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    // Определить результат
+    val result: String = if (userChoice == gameChoice) "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper")
+    ) "You win!"
+    else "You lose!"
+    // Вывести результат
+    println("You chose $userChoice. I chose $gameChoice. $result")
+}
